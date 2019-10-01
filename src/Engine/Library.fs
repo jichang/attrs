df2144e5a6361465567b56fa6ee35db400da01a8
Jacob Chang <mrchangji@outlook.com>
2019-10-01 09:10:52 +0800

    implement sign in and application import logic

--- src/Engine/Library.fs
+++ src/Engine/Library.fs
@@ -8,24 +8,27 @@ open System.Threading.Tasks
 open FSharp.Control.Tasks.V2
 
 module Host =
-    type IAppGrain =
-        inherit Orleans.IGrainWithGuidKey
-
-    type AppGrain() =
-        inherit Grain ()
+    type User =
+        { id: int64
+          flags: Map<string, bool>
+          props: Map<string, string> }
 
     type IUserGrain = 
-        inherit Orleans.IGrainWithIntegerKey
+        inherit Orleans.IGrainWithGuidKey
+        abstract member Query : unit -> Task<User>
         abstract member UpdateFlag : string -> bool -> Task<bool>
         abstract member UpdateProp : string -> string -> Task<bool>
 
-    type UserGrain() =
+    type UserGrain(id: int64) =
         inherit Grain ()
 
         let mutable flags = Map.empty
         let mutable props = Map.empty
 
-        interface IUserGrain with 
+        interface IUserGrain with
+            member __.Query () : Task<User> =
+                Task.FromResult { id = id; flags = flags; props = props }
+
             member __.UpdateFlag (key : string) (value: bool) : Task<bool> =
                 flags <- Map.add key value flags
                 Task.FromResult true
@@ -34,29 +37,123 @@ module Host =
                 props <- Map.add key value props
                 Task.FromResult true
 
+    type App =
+        { id: int64
+          name: string
+          status: int }
+
+    type IAppGrain =
+        inherit Orleans.IGrainWithGuidCompoundKey
+
+        abstract member QueryUsers: unit -> Task<User list>
+        abstract member AddUsers: User list -> Task<bool list>
+        abstract member AddUser: User -> Task<bool>
+        abstract member DelUser: User -> Task<bool>
+
+    type AppGrain() =
+        inherit Grain ()
+
+        let mutable users: User seq = Seq.empty
+
+        interface IAppGrain with
+            member __.QueryUsers () =
+                users
+                |> Seq.toList
+                |> Task.FromResult
+
+            member __.AddUsers newUsers =
+                newUsers
+                |> List.map
+                    (fun user ->
+                        match Seq.tryFind (fun (_user: User) -> _user.id = user.id) users with
+                        | Some _ ->
+                            false
+                        | None ->
+                            users <- Seq.append [user] users
+                            true
+                    )
+                |> Task.FromResult
+
+            member __.AddUser user =
+                match Seq.tryFind (fun (_user: User) -> _user.id = user.id) users with
+                | Some _ ->
+                    Task.FromResult false
+                | None ->
+                    users <- Seq.append [user] users
+                    Task.FromResult true
+
+            member __.DelUser user =
+                users <- Seq.filter (fun _user -> _user.id <> user.id) users
+                Task.FromResult true
+
+    type IClientGrain =
+        inherit Orleans.IGrainWithGuidKey
+
+        abstract member QueryApps: unit -> Task<App list>
+        abstract member AddApps: App list -> Task<bool list>
+        abstract member AddApp: App -> Task<bool>
+        abstract member DelApp: App -> Task<bool>
+
+    type ClientGrain() =
+        inherit Grain ()
+        let mutable apps: Map<int64, App> = Map.empty
+
+        interface IClientGrain with
+            member __.QueryApps () =
+                Map.toList apps
+                |> List.map snd
+                |> Task.FromResult
+
+            member __.AddApps newApps =
+                newApps
+                |> List.map
+                    (fun app ->
+                        match Map.tryFind app.id apps with
+                        | Some _ ->
+                            false
+                        | None ->
+                            apps <- Map.add app.id app apps
+                            true
+                    )
+                |> Task.FromResult
+
+            member __.AddApp app =
+                match Map.tryFind app.id apps with
+                | Some _ ->
+                    Task.FromResult false
+                | None ->
+                    apps <- Map.add app.id app apps
+                    Task.FromResult true
+
+            member __.DelApp app =
+                apps <- Map.remove app.id apps
+                Task.FromResult true
+
     let buildSiloHost () =
         let builder = SiloHostBuilder()
+        let assembly = Reflection.Assembly.GetExecutingAssembly()
         builder
             .UseLocalhostClustering()
             .ConfigureApplicationParts(fun parts ->
-                parts.AddApplicationPart(typeof<UserGrain>.Assembly)
-                      .AddApplicationPart(typeof<IUserGrain>.Assembly)
+                parts.AddApplicationPart(assembly)
                       .WithCodeGeneration() |> ignore)
             .Build()
 
-    let start name =
+    let start () =
         let t = task {
             let host = buildSiloHost ()
             do! host.StartAsync ()
 
-            printfn "Press any keys to terminate..."
-            Console.Read() |> ignore
-
-            do! host.StopAsync()
-
-            printfn "SiloHost is stopped"
+            return host
         }
 
+        t.Wait()
+        t.Result
+
+    let stop (host: ISiloHost) =
+        let t = task {
+            do! host.StopAsync()
+        }
         t.Wait()
 
 module Client =
@@ -67,3 +164,20 @@ module Client =
           .UseLocalhostClustering()
           .ConfigureApplicationParts(fun parts -> parts.AddApplicationPart(assembly).WithCodeGeneration() |> ignore )
           .Build()
+
+    let start () =
+        let t = task {
+            let client = buildClient()
+            do! client.Connect()
+
+            return client
+        }
+        t.Wait()
+        t.Result
+
+    let stop (client: IClusterClient) =
+        let t = task {
+            do! client.Close()
+        }
+
+        t.Wait()
--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,28 +1,203 @@
 ﻿open System
+open System.IO
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open FSharp.Control.Tasks.V2.ContextInsensitive
+
 open Giraffe
+open Feblr.Attrs
+open Feblr.Attrs.OAuth
+open Hopac
+
+open Orleans
+open Feblr.Attrs.Engine
+open Feblr.Attrs.Engine.Host
+
+module View =
+    open GiraffeViewEngine
+
+    let layout (content: XmlNode list) =
+        html [] [
+            head [] [
+                title []  [ encodedText "Feblr Attrs" ]
+                link [ _rel  "stylesheet"
+                       _type "text/css"
+                       _href "/styles/main.css" ]
+            ]
+            body [] content
+        ]
+
+    let signinView (oauthCfg: OAuthConfig) =
+        let link = sprintf "%s?client_id=%s&server_id=%s&scope_name=%s&redirect_uri=%s&response_type=code&state=%s" oauthCfg.endpoint oauthCfg.clientId oauthCfg.serverId oauthCfg.scopeName oauthCfg.redirectUri oauthCfg.state
+        let linkBtn = a [ _href link ] [ encodedText "Sign In"]
+        [ div [ _class "form--signin text-align--center container" ] [ h2 [] [ encodedText "Welcome to Feblr Attrs" ]; linkBtn ]]
+        |> layout
+
+    let importLink (oauthCfg: OAuthConfig) : string = 
+        sprintf "%s?client_id=%s&server_id=%s&scope_name=%s&redirect_uri=%s&response_type=code&state=%s" oauthCfg.endpoint oauthCfg.clientId oauthCfg.serverId oauthCfg.scopeName oauthCfg.redirectUri oauthCfg.state
+
+    let appRow (app: App) =
+        let linkBtn = a [_href (sprintf "/apps/%d" app.id) ] [encodedText "Details"]
+        tr []
+            [ td [ _class "text-align--right" ] [ encodedText (app.id.ToString()) ]
+              td [] [ encodedText app.name ] 
+              td [ _class "text-align--right" ] [ linkBtn ] ]
+
+    let appsTable (apps: App list) =
+        let tableHeader =
+            thead []
+                [ tr [] [ td [ _class "text-align--right" ] [ encodedText "Id" ]; td [ _class "text-align--right" ] [ encodedText "Name" ]; td [] [] ] ]
+
+        let tableBody =
+            tbody [] (List.map appRow apps)
+
+        table [] [ tableHeader ; tableBody ]
+
+    let headerView (oauthCfg: OAuthConfig) =
+        let link = importLink { oauthCfg with scopeName = "user.apps"; state = "user.apps" }
+        let title = h3 [ _class "flex__item" ] [ encodedText "Feblr Attrs "]
+        let importBtn = a [ _href link ] [ encodedText "Import Apps" ]
+        header [] [ div [ _class "container" ] [ div [_class "flex__box"] [title; importBtn] ] ]
+
+    let indexView (oauthCfg: OAuthConfig) (apps: App list) =
+        [ headerView oauthCfg; div [ _class "container" ] [appsTable apps] ]
+        |> layout
+
+    let appView (app: App) (users: User seq) =
+        []
+        |> layout
+
+module Handler =
+    let index (oauthCfg: OAuthConfig) =
+        fun (next: HttpFunc) (ctx: HttpContext) ->
+            task {
+                let openId = ctx.Session.GetString "open_id"
+                match openId with
+                | null ->
+                    let view = View.signinView { oauthCfg with scopeName = "user.id"; state = "user.id" }
+                    return! htmlView view next ctx
+                | _ ->
+                    let clusterClient = ctx.GetService<IClusterClient>()
+                    let openId = Guid.Parse (openId)
+                    let clientGrain = clusterClient.GetGrain<Host.IClientGrain>(openId)
+                    let! apps = clientGrain.QueryApps ()
+                    let view = View.indexView { oauthCfg with scopeName = "user.apps"; state = "user.apps" } apps
+                    return! htmlView view next ctx
+            }
+
+    let oauth (oauthCfg: OAuthConfig) : HttpHandler =
+        fun (next : HttpFunc) (ctx : HttpContext) ->
+            task {
+                match ctx.TryGetQueryStringValue "code", ctx.TryGetQueryStringValue "state" with
+                | None, _
+                | _, None ->
+                    return! redirectTo false "/code" next ctx
+                | Some code, Some state ->
+                    match state with
+                    | "user.id" ->
+                        let ticket =
+                            OAuth.oauth code oauthCfg
+                            |> run
+                        match ticket with
+                        | Ok ticket ->
+                            ctx.Session.SetString("open_id", ticket.open_id)
+                            return! redirectTo false "/" next ctx
+                        | Error error ->
+                            return! redirectTo false "/" next ctx
+                    | "user.apps" ->
+                        let ticket =
+                            OAuth.oauth code oauthCfg
+                            |> run
+                        match ticket with
+                        | Ok ticket ->
+                            let queryResult =
+                                OAuth.queryApps "http://sso.feblr.org/api/v1/applications" ticket
+                                |> run
+                            match queryResult with
+                            | Ok apps ->
+                                let clusterClient = ctx.GetService<IClusterClient>()
+                                let openId = Guid.Parse (ticket.open_id)
+                                let clientGrain = clusterClient.GetGrain<Host.IClientGrain>(openId)
+                                let! res = clientGrain.AddApps apps
+                                return! redirectTo false "/" next ctx
+                            | _ ->
+                                return! redirectTo false "/" next ctx
+                        | Error error ->
+                            return! redirectTo false "/" next ctx
+                    | _ ->
+                        return! redirectTo false "/" next ctx
+            }
+
+    let app (appId: int64) : HttpHandler =
+        fun (next : HttpFunc) (ctx : HttpContext) ->
+            task {
+                return! redirectTo false "/code" next ctx
+            }
 
 let webApp =
+    let oauthCfg =
+        { endpoint = "http://sso.feblr.org/oauth"
+          serverId = "f9be1b35c13a394cd7fbda5e38be80fbf68d5c685e65ada2c41f9f46832cc8fbb889211e1e9723e3115e4adecd097ddab019515e02848f2d5d9fcfeb0cec3892"
+          clientId = "1e9f7ef873b9141cd22e6f3e6e9e59295eaeb3f38594e0c8f449240dbc6ff90a20473baf3d3e96bbf55bc7517a5f99b7d6e0ab9aa3530fc1bc568db2c9e6027a"
+          clientSecret = "0a07328bf2ab1121466878d304acd950b894f254cd91c0537ff986b0fe109b7b304ce96e39084e3eb54bdda8d026f05e0b5ca658a79d77b09fd891ebd49b1971271da6128250584da9bfd4a8273d19018bf86586fa7d3167f3afec05aae91d6f9911e1b64846c5c6937c36446113b070dced8e376d10d2dc2367e62ddc76bf0f"
+          redirectUri = "http://attrs.feblr.org/oauth"
+          scopeName = ""
+          state = ""
+          ticketEndpoint = "http://sso.feblr.org/api/v1/tickets" }
     choose [
-        route "/api/v2/token"   >=> text "pong"
-        route "/api/v1/attrs"       >=> htmlFile "/pages/index.html" ]
+        GET >=> route "/" >=> Handler.index oauthCfg
+        GET >=> route "/oauth" >=> Handler.oauth oauthCfg
+        GET >=> routef "/apps/%d" Handler.app ]
 
 let configureApp (app : IApplicationBuilder) =
-    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseStaticFiles() |> ignore
+    app.UseSession() |> ignore
     app.UseGiraffe webApp
 
-let configureServices (services : IServiceCollection) =
-    // Add Giraffe dependencies
-    services.AddGiraffe() |> ignore
+let configureServices (client: IClusterClient) =
+    fun (services : IServiceCollection) ->
+        services.AddSingleton<IClusterClient>(client) |> ignore
+        services.AddDistributedMemoryCache() |> ignore
+        services.AddSession (fun options ->
+            options.IdleTimeout <- TimeSpan.FromSeconds (60.0 * 60.0 * 24.0)
+            options.Cookie.HttpOnly <- true
+            options.Cookie.IsEssential <- true
+        ) |> ignore
+        services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    // Set a logging filter (optional)
+    let filter (l : LogLevel) = l.Equals LogLevel.Error
+
+    // Configure the logging factory
+    builder.AddConsole()      // Set up the Console logger
+           .AddDebug()        // Set up the Debug logger
+
+           // Add additional loggers if wanted...
+    |> ignore
 
 [<EntryPoint>]
 let main _ =
+    let host = Host.start ()
+    let client = Client.start ()
+
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
     WebHostBuilder()
         .UseKestrel()
+        .UseContentRoot(contentRoot)
+        .UseWebRoot(webRoot)
+        .UseUrls("http://localhost:6060")  
         .Configure(Action<IApplicationBuilder> configureApp)
-        .ConfigureServices(configureServices)
+        .ConfigureServices(configureServices client)
+        .ConfigureLogging(configureLogging)
         .Build()
         .Run()
+
+    Client.stop client
+    Host.stop host
+
     0
