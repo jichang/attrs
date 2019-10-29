ee0d35d419b01da059a9c176b60febf5a0fe6625
Jacob Chang <mrchangji@outlook.com>
2019-10-30 00:08:03 +0800

    add rule checking to app grain

--- src/Engine/Library.fs
+++ src/Engine/Library.fs
@@ -12,171 +12,192 @@ module Host =
           flags: Map<string, bool>
           props: Map<string, string> }
 
-    type IUserGrain = 
-        inherit Orleans.IGrainWithGuidKey
-        abstract member Query : unit -> Task<User>
-        abstract member UpdateFlag : string -> bool -> Task<bool>
-        abstract member UpdateProp : string -> string -> Task<bool>
+    type IUserGrain =
+        inherit Orleans.IGrainWithGuidCompoundKey
+        abstract Query: unit -> Task<User>
+        abstract UpdateFlag: string -> bool -> Task<bool>
+        abstract QueryFlag: string -> Task<bool option>
+        abstract UpdateProp: string -> string -> Task<bool>
+        abstract QueryProp: string -> Task<string option>
 
     type UserGrain(id: int64) =
-        inherit Grain ()
+        inherit Grain()
 
         let mutable flags = Map.empty
         let mutable props = Map.empty
 
         interface IUserGrain with
-            member __.Query () : Task<User> =
-                Task.FromResult { id = id; flags = flags; props = props }
 
-            member __.UpdateFlag (key : string) (value: bool) : Task<bool> =
+            member __.Query(): Task<User> =
+                Task.FromResult
+                    { id = id
+                      flags = flags
+                      props = props }
+
+
+
+
+            member __.UpdateFlag (key: string) (value: bool): Task<bool> =
                 flags <- Map.add key value flags
                 Task.FromResult true
 
-            member __.UpdateProp (key : string) (value: string) : Task<bool> = 
+
+            member __.QueryFlag(key: string): Task<bool option> = Map.tryFind key flags |> Task.FromResult
+
+
+            member __.UpdateProp (key: string) (value: string): Task<bool> =
                 props <- Map.add key value props
                 Task.FromResult true
+
+            member __.QueryProp(key: string): Task<string option> = Map.tryFind key props |> Task.FromResult
 
     type App =
         { id: int64
           name: string
           status: int }
 
-    type IAppGrain =
-        inherit Orleans.IGrainWithGuidCompoundKey
+    type Condition =
+        | LastDigitsEqual of int * int64
+        | LastDigitsRange of int * int64 * int64
 
-        abstract member QueryUsers: unit -> Task<User list>
-        abstract member AddUsers: User list -> Task<bool list>
-        abstract member AddUser: User -> Task<bool>
-        abstract member DelUser: User -> Task<bool>
+    type Rule =
+        { value: bool
+          condition: Condition }
+
+        member this.Check(userId: int64) =
+            match this.condition with
+            | LastDigitsEqual(digits, lastDigits) -> (userId % pown 10L digits) = lastDigits
+            | LastDigitsRange(digits, lowerLimit, upperLimit) ->
+                let lastDigits = (userId % pown 10L digits)
+                lastDigits >= lowerLimit && lastDigits < upperLimit
+
+    type Feature =
+        { key: string
+          rules: Rule list }
+
+        member this.Check(userId: int64) =
+            List.fold (fun (value: bool) (rule: Rule) -> value && rule.Check userId) false this.rules
+
+    type IAppGrain =
+        inherit Orleans.IGrainWithGuidKey
+
+        abstract AddFeature: Feature -> Task<bool>
+        abstract DelFeature: Feature -> Task<bool>
+        abstract CheckFeature: int64 -> string -> Task<bool>
+
+        abstract AddUser: int64 -> Task<bool>
+        abstract DelUser: int64 -> Task<bool>
 
     type AppGrain() =
-        inherit Grain ()
+        inherit Grain()
 
-        let mutable users: User seq = Seq.empty
+        let mutable userIds: int64 list = List.empty
+        let mutable features: Feature list = List.empty
 
         interface IAppGrain with
-            member __.QueryUsers () =
-                users
-                |> Seq.toList
-                |> Task.FromResult
 
-            member __.AddUsers newUsers =
-                newUsers
-                |> List.map
-                    (fun user ->
-                        match Seq.tryFind (fun (_user: User) -> _user.id = user.id) users with
-                        | Some _ ->
-                            false
-                        | None ->
-                            users <- Seq.append [user] users
-                            true
-                    )
-                |> Task.FromResult
-
-            member __.AddUser user =
-                match Seq.tryFind (fun (_user: User) -> _user.id = user.id) users with
-                | Some _ ->
-                    Task.FromResult false
+            member __.AddFeature feature =
+                match List.tryFind (fun _feature -> _feature.key = feature.key) features with
+                | Some _ -> Task.FromResult false
                 | None ->
-                    users <- Seq.append [user] users
+                    features <- List.append [ feature ] features
                     Task.FromResult true
 
-            member __.DelUser user =
-                users <- Seq.filter (fun _user -> _user.id <> user.id) users
+            member __.DelFeature feature =
+                match List.tryFind (fun _feature -> _feature.key = feature.key) features with
+                | Some _ -> Task.FromResult false
+                | None ->
+                    features <- List.append [ feature ] features
+                    Task.FromResult true
+
+            member __.CheckFeature userId featureKey =
+                match List.tryFind (fun feature -> feature.key = featureKey) features with
+                | Some feature -> feature.Check userId |> Task.FromResult
+                | None -> Task.FromResult false
+
+            member __.AddUser userId =
+                match List.tryFind (fun _userId -> _userId = userId) userIds with
+                | Some _ -> Task.FromResult false
+                | None ->
+                    userIds <- List.append [ userId ] userIds
+                    Task.FromResult true
+
+            member __.DelUser userId =
+                userIds <- List.filter (fun _userId -> _userId <> userId) userIds
                 Task.FromResult true
 
     type IClientGrain =
         inherit Orleans.IGrainWithGuidKey
 
-        abstract member QueryApps: unit -> Task<App list>
-        abstract member AddApps: App list -> Task<bool list>
-        abstract member AddApp: App -> Task<bool>
-        abstract member DelApp: App -> Task<bool>
+        abstract QueryApps: unit -> Task<App list>
+        abstract AddApp: App -> Task<bool>
+        abstract DelApp: App -> Task<bool>
 
     type ClientGrain() =
-        inherit Grain ()
-        let mutable apps: Map<int64, App> = Map.empty
+        inherit Grain()
+
+        let mutable apps: App list = List.empty
 
         interface IClientGrain with
-            member __.QueryApps () =
-                Map.toList apps
-                |> List.map snd
-                |> Task.FromResult
 
-            member __.AddApps newApps =
-                newApps
-                |> List.map
-                    (fun app ->
-                        match Map.tryFind app.id apps with
-                        | Some _ ->
-                            false
-                        | None ->
-                            apps <- Map.add app.id app apps
-                            true
-                    )
-                |> Task.FromResult
+            member __.QueryApps() = Task.FromResult apps
 
             member __.AddApp app =
-                match Map.tryFind app.id apps with
-                | Some _ ->
-                    Task.FromResult false
+                match List.tryFind (fun (_app: App) -> _app.id = app.id) apps with
+                | Some _ -> Task.FromResult false
                 | None ->
-                    apps <- Map.add app.id app apps
+                    apps <- List.append [ app ] apps
                     Task.FromResult true
 
             member __.DelApp app =
-                apps <- Map.remove app.id apps
-                Task.FromResult true
+                match List.tryFind (fun (_app: App) -> _app.id = app.id) apps with
+                | Some _ ->
+                    apps <- List.filter (fun _app -> _app.id <> app.id) apps
+                    Task.FromResult true
+                | None -> Task.FromResult false
 
-    let buildSiloHost () =
+    let buildSiloHost() =
         let builder = SiloHostBuilder()
         let assembly = Reflection.Assembly.GetExecutingAssembly()
-        builder
-            .UseLocalhostClustering()
-            .ConfigureApplicationParts(fun parts ->
-                parts.AddApplicationPart(assembly)
-                      .WithCodeGeneration() |> ignore)
-            .Build()
+        builder.UseLocalhostClustering()
+               .ConfigureApplicationParts(fun parts -> parts.AddApplicationPart(assembly).WithCodeGeneration() |> ignore)
+               .Build()
 
-    let start () =
-        let t = task {
-            let host = buildSiloHost ()
-            do! host.StartAsync ()
+    let start() =
+        let t =
+            task {
+                let host = buildSiloHost()
+                do! host.StartAsync()
 
-            return host
-        }
-
+                return host
+            }
         t.Wait()
         t.Result
 
     let stop (host: ISiloHost) =
-        let t = task {
-            do! host.StopAsync()
-        }
+        let t = task { do! host.StopAsync() }
         t.Wait()
 
 module Client =
-    let buildClient () =
+    let buildClient() =
         let assembly = Reflection.Assembly.GetExecutingAssembly()
         let builder = ClientBuilder()
-        builder
-          .UseLocalhostClustering()
-          .ConfigureApplicationParts(fun parts -> parts.AddApplicationPart(assembly).WithCodeGeneration() |> ignore )
-          .Build()
+        builder.UseLocalhostClustering()
+               .ConfigureApplicationParts(fun parts -> parts.AddApplicationPart(assembly).WithCodeGeneration() |> ignore)
+               .Build()
 
-    let start () =
-        let t = task {
-            let client = buildClient()
-            do! client.Connect()
+    let start() =
+        let t =
+            task {
+                let client = buildClient()
+                do! client.Connect()
 
-            return client
-        }
+                return client
+            }
         t.Wait()
         t.Result
 
     let stop (client: IClusterClient) =
-        let t = task {
-            do! client.Close()
-        }
+        let t = task { do! client.Close() }
 
         t.Wait()
--- src/Server/OAuth.fs
+++ src/Server/OAuth.fs
@@ -1,4 +1,5 @@
 namespace Feblr.Attrs
+
 open System
 
 open Feblr.Attrs.Engine.Host
@@ -28,26 +29,26 @@ module OAuth =
           access_token: string
           refresh_token: string }
 
-    let oauth (code: string) (oauthCfg: OAuthConfig) = job {
-        let body =
-            {| code = code
-               client_id = oauthCfg.clientId
-               client_secret = oauthCfg.clientSecret |}
-        let json = Encode.Auto.toString(4, body)
-        let request =
-            Request.createUrl Post oauthCfg.ticketEndpoint
-            |> Request.bodyString json
-        use! response = getResponse request
-        let! bodyStr = Response.readBodyAsString response
-        return Decode.Auto.fromString<OAuthTicket>(bodyStr)
-    }
+    let oauth (code: string) (oauthCfg: OAuthConfig) =
+        job {
+            let body =
+                {| code = code
+                   client_id = oauthCfg.clientId
+                   client_secret = oauthCfg.clientSecret |}
+            let json = Encode.Auto.toString (4, body)
+            let request = Request.createUrl Post oauthCfg.ticketEndpoint |> Request.bodyString json
+            use! response = getResponse request
+            let! bodyStr = Response.readBodyAsString response
+            return Decode.Auto.fromString<OAuthTicket> (bodyStr)
+        }
 
-    let queryApps (url: string) (ticket: OAuthTicket) = job {
-        let request =
-            Request.createUrl Get url
-            |> Request.queryStringItem "open_id" ticket.open_id
-            |> Request.queryStringItem "access_token" ticket.access_token
-        use! response = getResponse request
-        let! bodyStr = Response.readBodyAsString response
-        return Decode.Auto.fromString<App list>(bodyStr, false, extraCoders)
-    }
+    let queryApps (url: string) (ticket: OAuthTicket) =
+        job {
+            let request =
+                Request.createUrl Get url
+                |> Request.queryStringItem "open_id" ticket.open_id
+                |> Request.queryStringItem "access_token" ticket.access_token
+            use! response = getResponse request
+            let! bodyStr = Response.readBodyAsString response
+            return Decode.Auto.fromString<App list> (bodyStr, false, extraCoders)
+        }
